3ce61fdf0d13b389c6e0632bdd798202e906465b
uacanada <mail@uacanada.org>
2023-08-15 21:19:16 -0700

    Fix modules

--- static/js/uacanadamap.js
+++ static/js/uacanadamap.js
@@ -1,32 +1,22 @@
-// "use strict";
-// define('uacanadamap', [
-//  "core/variables",
-// "core/initialization", 
-// ], function (
-//   UacanadaMap,  // This is uacanadamap/variables: Importing module to populate global UacanadaMap Obj with essential variables
-//   initialization, // Handles map initialization, loads dependencies, and augments the UacanadaMap with functions and listeners.
-//   // Built-in AMD modules:
-//   module 
-//   ) {
-
-
-// console.log('is ok')
-
-
-// UacanadaMap.needReinit = false
-// initialization(UacanadaMap)
-
-
-
-//   return UacanadaMap;
-// });
-
-
-
 "use strict";
-define('uacanadamap', function() {
+define('uacanadamap', [
+ "core/variables",
+"core/initialization", 
+], function (
+  UacanadaMap,  // This is uacanadamap/variables: Importing module to populate global UacanadaMap Obj with essential variables
+  initialization, // Handles map initialization, loads dependencies, and augments the UacanadaMap with functions and listeners.
+  // Built-in AMD modules:
+  module 
+  ) {
 
 
-console.log('ENTRYPOINT IS OKAY')
+console.log('is ok')
 
+
+UacanadaMap.needReinit = false
+initialization(UacanadaMap)
+
+
+
+  return UacanadaMap;
 });
