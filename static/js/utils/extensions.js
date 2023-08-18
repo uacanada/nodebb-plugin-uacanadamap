1f69fc0ab8506a3403c5fcbb5e1e9bc2573b910a
uacanada <mail@uacanada.org>
2023-08-18 15:12:17 -0700

    Small Fixes

--- static/js/utils/extensions.js
+++ static/js/utils/extensions.js
@@ -1,16 +1,14 @@
-'use strict';
-define('utils/extensions', ["core/variables" /*   Global object UacanadaMap  */], function(UacanadaMap) { 
-
-const extensions = (UacanadaMap) => {
-    UacanadaMap.console = {
-        log: (...args) => {
-          if (app.user.isAdmin) {
-            console.log(...args);
-          }
-        },
-      };
+"use strict";
+define("utils/extensions", [
+  "core/variables" /*   Global object UacanadaMap  */,
+], function (UacanadaMap) {
+  UacanadaMap.console = {
+    log: (...args) => {
+      if (app.user.isAdmin) {
+        console.log(...args);
+      }
+    },
   };
-  module.exports = { extensions };
-  
+
   return UacanadaMap;
-})
+});
