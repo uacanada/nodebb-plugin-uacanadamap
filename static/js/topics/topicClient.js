1dd2993dea7ddf2fbb4dc5946f8b45ccfe5b889e
uacanada <mail@uacanada.org>
2023-08-18 17:33:25 -0700

    topics/topicClient

--- static/js/topics/topicClient.js
+++ static/js/topics/topicClient.js
@@ -1,7 +1,7 @@
 "use strict";
 define('topics/topicClient',["core/variables" /*   Global object UacanadaMap  */], function(UacanadaMap) { 
 
-
+console.log('test')
 
 UacanadaMap.api.showMapInsideTopic = (place) => {
     const {L} = UacanadaMap
