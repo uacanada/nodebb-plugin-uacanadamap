3366edec67dd125c90e031f21d93047e291836d2
uacanada <mail@uacanada.org>
2023-11-24 20:57:14 -0800

    Multiple place images receiver (styles)

--- static/js/events/registerableListeners.js
+++ static/js/events/registerableListeners.js
@@ -65,10 +65,7 @@ class EventListeners {
 					let initMainMark = i === 0 ? 'mainPlaceImg' : ''
 				
 					$("#ua-form-img-holder").append(
-						'<div class="image-preview d-flex flex-column align-items-center m-2">' +
-						'<img src="' + data + '" class="set-main-image '+initMainMark+'" data-image-index="' + i + '">' +
-					//	'<button title="main" class="set-main-image btn btn-primary btn-sm mt-1" data-image-index="' + i + '"><i class="fa-solid fa-id-badge"></i></button>' +
-						'</div>'
+						'<div data-image-index="' + i + ' class="image-preview d-flex flex-column align-items-center m-2 '+initMainMark+'"><img src="' + data + '"></div>'
 					);
 					
 				};
@@ -77,10 +74,10 @@ class EventListeners {
 		});
 		
 		
-		$(document).on('click', '.set-main-image', function() {
-			$('.set-main-image').removeClass('mainPlaceImg')
+		$(document).on('click', '.image-preview', function() {
+			$('.image-preview').removeClass('mainPlaceImg')
 			const imageIndex = $(this).data('image-index');
-			$('.set-main-image[data-image-index="'+imageIndex+'"]').addClass('mainPlaceImg')
+			$('.image-preview[data-image-index="'+imageIndex+'"]').addClass('mainPlaceImg')
 			$('#mainImage').val(imageIndex);
 			
 		});
